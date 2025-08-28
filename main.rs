6ad8c960ce14cf266d6c9fe00f8fe49427a462b6
PolshikRust <polshynskyi.uber@gmail.com>
2025-08-28 15:28:59 +0200

    Update main.rs

--- main.rs
+++ main.rs
@@ -2,8 +2,11 @@ fn main() {
     let player_name = String::from("Aragorn");
     let player_level = 15;
     generate_report(&player_name, &player_level);
+
     println!("Welcome to the main hall {}!", player_name);
+    println!("This is a new line!"); // <-- НАША НОВАЯ СТРОКА
 }
-fn generate_report(name: &String, level: &u32){
-println!("Report: Player {} is level {}.", name, level);}
-   
+
+fn generate_report(name: &String, level: &u32) {
+    println!("Report: Player {} is level {}.", name, level);
+}
