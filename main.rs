1a3185e852bebce7f5e14a52fa935da7f1a28be9
PolshikRust <polshynskyi.uber@gmail.com>
2025-08-29 12:20:44 +0200

    New functions
    Impl
    Self
    Mut self
    &
    String
    Struct

--- main.rs
+++ main.rs
@@ -3,15 +3,36 @@ struct InventoryItem {
     quantity: u32,
     description: String,
 }
+
+impl InventoryItem {
+    fn format_item_details(&self) -> String {
+    format!("--- Item Details ---\nName: {}\nQuantity: {}\nDescription: {}", self.name, self.quantity, self.description)
+    }
+
+
+fn use_item(&mut self) {
+    if self.quantity > 0 {
+        self.quantity -= 1; // Decrease quantity 
+        println!("Used one {}. Remaining: {}.", self.name, self.quantity);
+    } else {
+        println!("Cannot use {}. None left!", self.name);
+    }
+    }}
+ 
+
 fn main(){
-    let item = InventoryItem {
+    let mut item = InventoryItem {
         name: String::from("Health Potion"),
         quantity: 5,
         description: String::from("A simple potion that restores 50 HP")};
-        let item_details = format_item_details(&item);
-        println!("{}", item_details);
+        let initial_details = item.format_item_details();
+         println!("{}", initial_details);
+         println!("\n--- Using Items ---");
+         item.use_item();
+         item.use_item();
+         item.use_item();
+         println!("\n--- Final State ---");
+         let final_details = item.format_item_details();
+         println!("{}", final_details);
+
     }
-fn format_item_details(inventoryitem: &InventoryItem) -> String {
-    let details = format!("--- Item Details ---\nName: {}\nQuantity: {}\nDescription: {}", inventoryitem.name, inventoryitem.quantity, inventoryitem.description);
-    return details;
-}
